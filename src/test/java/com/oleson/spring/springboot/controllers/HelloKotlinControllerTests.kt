74f722b74610c186a9aded1a269a35fe23d93fa0
Christian Oleson <olesoncs@gmail.com>
2025-06-21 22:14:19 -0500

    Add some tests

--- src/test/java/com/oleson/spring/springboot/controllers/HelloKotlinControllerTests.kt
+++ src/test/java/com/oleson/spring/springboot/controllers/HelloKotlinControllerTests.kt
@@ -1,5 +1,7 @@
 package com.oleson.spring.springboot.controllers
 
+import com.oleson.spring.springboot.services.buildingJavaPrograms.Chapter2
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 import org.springframework.boot.test.context.SpringBootTest
@@ -8,12 +10,20 @@ import kotlin.test.assertEquals
 
 @SpringBootTest
 class HelloKotlinControllerTests {
-    private val SUT = HelloKotlinController()
+
+    private lateinit var sut: HelloKotlinController
+    private lateinit var chapter2: Chapter2
+
+    @BeforeEach
+    fun setUp() {
+        chapter2 = Chapter2()
+        sut = HelloKotlinController(chapter2)
+    }
 
     @Test
     fun testHello() {
         // Act
-        val result = SUT.hello()
+        val result = sut.hello()
 
         // Assert
         assert(result == "Hello from Kotlin!")
@@ -23,9 +33,33 @@ class HelloKotlinControllerTests {
    @ValueSource(strings = ["Alice", "Bob", "Charlie"])
    fun testGreeting(name: String) {
         // Act
-        val result = SUT.greeting(name)
+        val result = sut.greeting(name)
 
         // Assert
        assertEquals("Hello, $name!", result)
+    }
+
+    @ParameterizedTest(name = "{index} - Fibonacci({0})")
+    @ValueSource(ints = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    fun testFibonacci(number: Int) {
+        // Arrange
+        val fibonacci = chapter2.fibonacci(number)
+
+        // Act
+        val result = sut.fibonacci(number)
+
+        // Assert
+        assert(result.statusCode.is2xxSuccessful)
+        assertEquals(fibonacci, result.body)
+    }
+
+    @Test
+    fun invalidFibonacci() {
+        // Act
+        val result = sut.fibonacci(-1)
+
+        // Assert
+        assert(result.statusCode.is4xxClientError)
+        assert(result.body == null)
     }
 }
---
+++ src/test/java/com/oleson/spring/springboot/services/Chapter2Tests.kt
@@ -0,0 +1,4 @@
+package com.oleson.spring.springboot.services
+
+class Chapter2Tests {
+}
